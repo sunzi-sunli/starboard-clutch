e9cea4150dfde9a24dcb4ada033867f07da8b83f
sunli <sunzi.sunli@gmail.com>
2019-09-15 15:10:36 +1000

    first few sources and exes

---
+++ source/calculator/add.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main(int argc, char *argv[]){
+	int i,j;
+
+	if(argc!=3){
+		return 1;
+	}
+	i=atol(argv[1]);
+	j=atol(argv[2]);
+	printf("%d\n", i+j);
+	return 0;
+}
