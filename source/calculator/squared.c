a1367b45f7ad3f8b797cc117ecf1f38f62774d58
sunli <sunzi.sunli@gmail.com>
2019-09-23 16:29:43 +1000

    new additional programs getting somewhere

---
+++ source/calculator/squared.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int
+main(int argc, char *argv[]){
+	int a;
+	int answer;
+
+	if(argc!=2){
+		return 1;
+	}
+
+	a=atoi(argv[1]);
+	answer=a*a;
+	printf("%d\n", answer);
+	return 0;
+}
